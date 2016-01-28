e17942870125eeb93eac4c04b0b3721b748e7959
Iffar <galla.adam@gmail.com>
2016-01-28 11:12:03 +0100

    fix

--- ServerSide.js
+++ ServerSide.js
@@ -374,9 +374,7 @@ handlers.CheckProgress = function ( args )
 					
 					
 					var storedMaterials = 0
-					log += "\n"+buildingInstance.CustomData.StoredMaterial;
-					
-					if( typeof buildingInstance.CustomData.StoredMaterial == 'undefined')
+					if( typeof buildingInstance.CustomData.StoredMaterial != 'undefined')
 						storedMaterials = parseInt(buildingInstance.CustomData.StoredMaterial);
 						
 					log += "\n"+storedMaterials+" + "+amount+" <= "+storage;
